475f4341c01b57368aa0eafb74f1ec4dfaeee530
junghwa5 <50373166+junghwa5@users.noreply.github.com>
2022-11-28 17:14:17 +0900

    Add files via upload

---
+++ hackerrank_NewCompanies_20221128.sql
@@ -0,0 +1,8 @@
+SELECT c.company_code, c.founder, COUNT(DISTINCT(l.lead_manager_code)), COUNT(DISTINCT(s.senior_manager_code)), COUNT(DISTINCT(m.manager_code)), COUNT(DISTINCT(e.employee_code))
+FROM Company as c
+    JOIN Lead_Manager as l ON c.company_code = l.company_code
+    JOIN Senior_Manager as s ON l.lead_manager_code = s.lead_manager_code
+    JOIN Manager as m ON s.senior_manager_code = m.senior_manager_code
+    JOIN Employee as e ON m.manager_code = e.manager_code
+GROUP BY c.company_code, c.founder
+ORDER BY c.company_code;
